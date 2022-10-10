cf15180cd9df64585c0b48674cf9b11282e9d41a
andrejsPicilevics <jelena.picilevica@gmail.com>
2022-10-11 00:40:54 +0300

    1st commit by Jelena

--- src/main/java/Lena.java
+++ src/main/java/Lena.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
+import java.sql.*;
 import java.util.Scanner;
 
 public class Lena {
@@ -8,7 +6,7 @@ public class Lena {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurant", "root", "jelenaandrejs");
-            reserveTable(connection);
+           viewAllMealsByType(connection);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -35,5 +33,66 @@ public class Lena {
             e.printStackTrace();
         }
 
+        System.out.println("Table No" + tableNum + " was reserved.");
+
     }
+
+    public static void unreserveTable(Connection connection) {
+
+        System.out.println("Enter number of table you want to unreserve: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int tableNum = scanner.nextInt();
+
+        String reserve = "UPDATE restaurant.tables SET table_reserved = false WHERE id = ?";
+
+        try {
+            PreparedStatement pStatement = connection.prepareStatement(reserve);
+            pStatement.setInt(1, tableNum);
+
+            pStatement.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Table No" + tableNum + " was unreserved.");
+
+    }
+
+    public static void  getAllMeals (Connection connection) {
+        String getMeals = "SELECT id, meal_title, price FROM restaurant.meals;";
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getMeals);
+
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString(1));
+                System.out.print(". " + resultSet.getString(2));
+                System.out.println(", price: " + resultSet.getString(3));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void  viewAllMealsByType (Connection connection) {
+        String viewMeals = "SELECT meal_type, meal_title FROM restaurant.meals INNER JOIN restaurant.meal_type ON meal_type.id = meals.meal_type_id ORDER BY meal_type";
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(viewMeals);
+
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString(1));
+                System.out.println(". " + resultSet.getString(2));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
