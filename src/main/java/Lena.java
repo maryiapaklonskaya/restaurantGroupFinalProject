308cd6a7e891f3fc3646ea04c2eaab2f59a0773f
andrejsPicilevics <jelena.picilevica@gmail.com>
2022-10-10 23:43:58 +0300

    1st commit by Jelena

--- src/main/java/Lena.java
+++ src/main/java/Lena.java
@@ -1,2 +1,39 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
+
 public class Lena {
+
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurant", "root", "jelenaandrejs");
+            reserveTable(connection);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void reserveTable(Connection connection) {
+
+        System.out.println("Enter number of table you want to reserve: ");
+
+        Scanner scanner = new Scanner(System.in);
+        int tableNum = scanner.nextInt();
+
+        String reserve = "UPDATE restaurant.tables SET table_reserved = true WHERE id = ?";
+
+        try {
+            PreparedStatement pStatement = connection.prepareStatement(reserve);
+            pStatement.setInt(1, tableNum);
+
+            pStatement.executeUpdate();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/Orders.java
+++ src/main/java/Orders.java
@@ -6,12 +6,14 @@ public class Orders {
     public static void main(String[] args){
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurant", "root", "rootroot");
-            //createUser(connection);
+
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
+
 
 }
 
