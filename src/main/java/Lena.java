09e115c2f6b1764ac3d908e9b187057033410e1e
andrejsPicilevics <jelena.picilevica@gmail.com>
2022-10-11 01:37:02 +0300

    7 method written by Jelena

--- src/main/java/Lena.java
+++ src/main/java/Lena.java
@@ -6,7 +6,7 @@ public class Lena {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/restaurant", "root", "jelenaandrejs");
-           viewAllMealsByType(connection);
+          getAllOrders(connection);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -93,6 +93,95 @@ public class Lena {
             e.printStackTrace();
         }
     }
+
+    public static void  getAllWaiters (Connection connection) {
+        String getWaiters = "SELECT * FROM restaurant.waiters";
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getWaiters);
+
+            while (resultSet.next()) {
+                System.out.print(resultSet.getString(1));
+                System.out.print(". " + resultSet.getString(2));
+                System.out.println(" " + resultSet.getString(3));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addWaiter(Connection connection){
+
+        System.out.println("Please enter waiter name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter waiter surname: ");
+        String surname = scanner.nextLine();
+
+        String addWaiter = "INSERT INTO restaurant.waiters (name, surname) VALUES (?, ?)" ;
+
+        try{
+            PreparedStatement pStatement = connection.prepareStatement(addWaiter);
+            pStatement.setString(1, name);
+            pStatement.setString(2, surname);
+
+            pStatement.executeUpdate();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Waiter " + name + " " + surname + " has been added.");
+
+    }
+
+    public static void removeWaiter(Connection connection){
+
+        System.out.println("Please enter waiter id: ");
+        Scanner scanner = new Scanner(System.in);
+        int id = scanner.nextInt();
+
+        String addWaiter = "DELETE FROM restaurant.waiters WHERE id = ?" ;
+
+        try{
+            PreparedStatement pStatement = connection.prepareStatement(addWaiter);
+            pStatement.setInt(1, id);
+
+
+            pStatement.executeUpdate();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+        System.out.println("Waiter with ID " + id + " has been removed.");
+
+    }
+
+    public static void  getAllOrders (Connection connection) {
+        String getOrders = "SELECT restaurant.orders.id AS 'Order_id', restaurant.orders.tables_id, restaurant.orders.status, SUM(meals.price*quantity_of_meals) AS 'Total_Amount' \n" +
+                "FROM restaurant.orders LEFT JOIN restaurant.orders_items ON orders.id = restaurant.orders_items.id" +
+                " INNER JOIN restaurant.meals ON orders_items.meal_id = meals.id ORDER BY orders.id;";
+
+
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(getOrders);
+
+            while (resultSet.next()) {
+                System.out.print("Order ID: " + resultSet.getInt(1));
+                System.out.print("  Table ID: " + resultSet.getInt(2));
+                System.out.print("  Status: " + resultSet.getString(3));
+                System.out.println("  Total amount: " + resultSet.getDouble(4));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
